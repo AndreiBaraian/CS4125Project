e886a97c8d8dfd3c6554a0b6e1100a69120f3217
Lucian Epure <lucian.epure@yahoo.com>
2017-10-25 13:42:49 +0100

    luci commit

---
+++ src/main/Main.java
@@ -0,0 +1,34 @@
+package main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import dao.SessionFactoryUtil;
+import model.Service;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Service s1 = new Service();
+		Service s2 = new Service();
+		s1.setName("Call");
+		s2.setName("Mobile Data");
+		s1.setId(3L);
+		s2.setId(5L);
+		s1.setRegion("Romania");
+		s2.setRegion("France");
+		
+		SessionFactory sessionFactory = SessionFactoryUtil.getInstance();
+		Session session = sessionFactory.openSession();
+		Transaction tx = null;
+		tx = session.beginTransaction();
+		session.save(s1);
+		session.save(s2);
+		tx.commit();
+		session.disconnect();
+		
+	}
+
+}

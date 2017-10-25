37526336e50e477e2687f46025b204d454b1dc82
AndreiBaraian <andreibaraian88@gmail.com>
2017-10-25 13:22:23 +0100

    initial commit

---
+++ src/dao/SessionFactoryUtil.java
@@ -0,0 +1,17 @@
+package dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryUtil {
+	
+	private static SessionFactory sessionFactory = null;
+	
+	public static SessionFactory getInstance() {
+		if(sessionFactory == null) {
+			sessionFactory = (new Configuration()).configure().buildSessionFactory();
+		}
+		return sessionFactory;
+	}
+
+}

cdc44414bc16413efa6ff581eb49f9951dbfc07f
AndreiBaraian <andreibaraian88@gmail.com>
2017-11-14 16:16:26 +0000

    working add in accountBLL

--- src/insertions/DataInsert.java
+++ src/insertions/DataInsert.java
@@ -1,7 +1,10 @@
 package insertions;
 
+import account.EnterpriseAccount;
+import bll.AccountBLL;
 import bll.CustomerBLL;
 import bll.EnterpriseAccountBLL;
+import customer.Customer;
 import exceptions.InsertException;
 import region.Region;
 import region.Romania;
@@ -9,16 +12,17 @@ import region.Romania;
 public class DataInsert {
 	
 	private CustomerBLL customerBLL;
-	private EnterpriseAccountBLL enterpriseAccountBLL;
+	private AccountBLL<EnterpriseAccount> enterpriseBLL;
 	
 	public DataInsert(){
 		this.customerBLL = new CustomerBLL();
+		enterpriseBLL = new EnterpriseAccountBLL();
 	}
 	
 	public void insertCustomersAndAccounts() throws InsertException{
 		Region romania = new Romania();
-		customerBLL.add("Andrei", "Baraian", 20, "Bihorului 14", "andrei@gmail.com");
-		
+		Customer customer1 = customerBLL.add("Andrei", "Baraian", 20, "Bihorului 14", "andrei@gmail.com");
+		enterpriseBLL.addAccount("Enterprise", romania, customer1, "0727455675", "12/11/2017");
 	}
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,55 +1,21 @@
 package main;
 
-import bll.AccountBLL;
-import bll.EnterpriseAccountBLL;
-import control.Control;
-import customer.Customer;
 import exceptions.InsertException;
-import gui.Menu;
-import region.Region;
-import region.Romania;
+import insertions.DataInsert;
 
 public class Main {
 
 	public static void main(String[] args) throws InsertException {
 		
-		
+		/*
 		Control c = Control.getInstance();
 		Menu window = new Menu();
 		window.getFrmMainMenu().setVisible(true);
-		
-		
-		/*
-		AccountBLL<?> e = new EnterpriseAccountBLL();
-		Region reg = new Romania();
-		Customer cust = new Customer("Andrei", "Baraian",21,"Bihorului","andrei@gmail.com");
-		cust.setSystemReference("ref4");
-		e.addAccount("Enterprise", reg, cust, "0004354353", "3DEC");
 		*/
 		
-		/*
-		AccountBLL<?> acb = new AccountBLL();
-		Account acc = acb.getAccount("072354345");
-		acc.setBalance(30);
-		acb.modifyAccount(acc);
-		System.out.println(acc.toString());
-		System.out.println(acc.getClass().getSimpleName());
-		*/
+		DataInsert dt = new DataInsert();
+		dt.insertCustomersAndAccounts();
 		
-		/*
-		CustomerBLL cb = new CustomerBLL();
-		Customer luci = cb.add("Lucian", "Epure", 20, "Observator", "luci@gmail.com");
-		Region romania = new Romania(23,23);
-		AccountBLL ac = new FamilyAccountBLL();
-		ac.addAccount("Family", romania, luci, "005463450", "34243242");
-		*/
-		
-		/*
-		Region romania = new Romania(23,23);
-		Service msg = new Call(romania, "05353450", romania, 50);
-		CallDAO md = new CallDAO();
-		md.add((Call)msg);
-		*/
 	}
 
 }

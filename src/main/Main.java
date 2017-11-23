ea88c1cd16b345dac83cdd3b44f99137047e1bd8
Xiangkai Tang <empathytxk@hotmail.com>
2017-11-23 12:23:28 +0000

    Fix some error in generatedServices

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 
+import insertions.DataInsert;
 import account.Account;
 import account.EnterpriseAccount;
 import computeTransaction.ComputeTransaction;
@@ -9,25 +10,27 @@ import computeTransaction.UpdateAccountContext;
 import control.Control;
 import customer.Customer;
 import exceptions.InsertException;
+import region.Romania;
+import service.Call;
 import view.Menu;
 
 public class Main {
 
 	public static void main(String[] args) throws InsertException {
 		
-		//DataInsert dt = new DataInsert();
+		DataInsert dt = new DataInsert();
 		//dt.insertCustomersAndAccounts();
-		//dt.insertRegions();
+		dt.insertRegions();
 		
 		//AccountBLL<?> accountBLL = new AccountBLL();
 		
 		
 		
-		/*
+		
 		Control control = Control.getInstance();
 		Menu window = new Menu();
 		window.getFrmMainMenu().setVisible(true);
-		*/
+		
 
 		
 		
